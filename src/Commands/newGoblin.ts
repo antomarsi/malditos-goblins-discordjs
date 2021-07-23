78dab13494186f402fbe42b1bfecf61a90c3a5aa
Antonio Marco <antonio.marco@liferaftinc.com>
2021-07-23 12:03:37 -0300

    using custom lib to generate goblin

--- src/Commands/newGoblin.ts
+++ src/Commands/newGoblin.ts
@@ -1,38 +1,40 @@
 import { Message, MessageEmbed, User } from 'discord.js'
-import Goblin from '../Goblin/index'
+import MalditosGoblins from 'malditos-goblins-lib'
 import { ICommand } from './index'
 const name = 'novo'
 
 const description = 'Cria um novo goblin'
 
 const formatMessageNewGoblin = (
-  goblin: Goblin,
+  goblin: MalditosGoblins.Goblin,
   author: User
 ): MessageEmbed => {
+
+  const specialFeature = `${goblin.specialFeature.name} - ${goblin.specialFeature.description}` + goblin.specialFeature.extraDescriptions ? `(${goblin.specialFeature.extraDescriptions?.join(", ")})` : ""
   const message = new MessageEmbed()
   message.setTitle(`Uma nova *Coisinha Verde©* (ou de outra cor) foi criada`)
   message.setAuthor(author.username)
-  message.setColor(goblin.coloracao.cor)
+  message.setColor(goblin.coloration.color)
   message.addFields(
-    { name: 'Nome gerado:', value: goblin.nome, inline: true },
-    { name: 'Ocupação:', value: goblin.ocupacao.nome, inline: true },
-    { name: 'Coloração:', value: goblin.coloracao.nome, inline: true },
-    { name: 'Caracteristica:', value: goblin.caracteristica.toString() }
+    { name: 'Nome gerado:', value: MalditosGoblins.generateName(), inline: true },
+    { name: 'Ocupação:', value: goblin.ocupation.name, inline: true },
+    { name: 'Coloração:', value: goblin.coloration.name, inline: true },
+    { name: 'Caracteristica:', value: specialFeature }
   )
-  message.addField(':crossed_swords: Combate', goblin.combate, true)
-  message.addField(':man_running: Habilidade', goblin.habilidade, true)
-  message.addField(':books: Conhecimento', goblin.conhecimento, true)
-  message.addField(':four_leaf_clover: Sorte', goblin.sorte, true)
+  message.addField(':crossed_swords: Combate', goblin.combat, true)
+  message.addField(':man_running: Habilidade', goblin.dexterity, true)
+  message.addField(':books: Conhecimento', goblin.knowledge, true)
+  message.addField(':four_leaf_clover: Sorte', goblin.luck, true)
 
-  goblin.ocupacao.habilidades.forEach((hab, index) => {
-    message.addField(`Level ${index + 1} - ${hab.nome}`, hab.descricao)
+  goblin.ocupation.skills.forEach((skill, index) => {
+    message.addField(`Level ${index + 1} - ${skill.name}`, skill.description)
   })
-  message.addField(`Equipamentos iniciais:`, goblin.ocupacao.equipamento)
+  message.addField(`Equipamentos iniciais:`, goblin.equipments.map(equip => equip.toString()).join("\n"))
   return message
 }
 
 const execute = (msg: Message, args: string[]) => {
-  const goblin = Goblin.generateGoblin()
+  const goblin = MalditosGoblins.generateGoblin()
   const message = formatMessageNewGoblin(goblin, msg.author)
   msg.channel.send(message)
 }
