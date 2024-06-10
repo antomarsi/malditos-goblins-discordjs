2194a436b3537a07170be643747ac1b2649b726c
Antonio Marco da Silva <antomarsi@hotmail.com>
2024-06-09 23:19:32 -0300

    mais uma vez

--- dist/events/main/ready.js
+++ dist/events/main/ready.js
@@ -9,9 +9,6 @@ exports.default = new Event_1.Event({
     run() {
         const { commands, buttons, selects, modals } = __1.client;
         console.log(`Commands   loaded: ${commands.size}`.cyan);
-        console.log(`Buttons    loaded: ${buttons.size}`.cyan);
-        console.log(`Selects    loaded: ${selects.size}`.cyan);
-        console.log(`Modals     loaded: ${modals.size}`.cyan);
         console.log("✅ Bot online".green);
     }
 });
--- src/events/main/ready.ts
+++ src/events/main/ready.ts
@@ -10,9 +10,6 @@ export default new Event({
         const { commands, buttons, selects, modals } = client;
 
         console.log(`Commands   loaded: ${commands.size}`.cyan)
-        console.log(`Buttons    loaded: ${buttons.size}`.cyan)
-        console.log(`Selects    loaded: ${selects.size}`.cyan)
-        console.log(`Modals     loaded: ${modals.size}`.cyan)
         console.log("✅ Bot online".green)
     }
 })
